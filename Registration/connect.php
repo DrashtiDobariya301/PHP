7e56256fbe825a0c8df3f2d1623d8893d125a2bf
Drashti Dobariya <114356720+DrashtiDobariya301@users.noreply.github.com>
2023-06-29 21:07:48 +0530

    Update connect.php

--- Registration/connect.php
+++ Registration/connect.php
@@ -1,1 +1,18 @@
+//In connect.php based on established the connection between the database and php files
 
+<?php
+//create a variable to connect with the database
+$HOSTNAME = 'localhost'; 
+$USERNAME = 'root';
+$PASSWORD = 'za81as@#1212';
+$DATABASE = 'signupforms';
+
+
+$con=mysqli_connect($HOSTNAME,$USERNAME,$PASSWORD,$DATABASE);
+
+if(!$con){
+    //show the error if the database is not connect 
+    die(mysqli_error($con));
+}
+
+?>
