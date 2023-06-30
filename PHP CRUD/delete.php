1efec9f3ee2168fa3a27bea78890437cd0906a28
Drashti Dobariya <114356720+DrashtiDobariya301@users.noreply.github.com>
2023-06-30 15:29:07 +0530

    Update delete.php

--- PHP CRUD/delete.php
+++ PHP CRUD/delete.php
@@ -1,1 +1,22 @@
+#In PHP CRUD D-Delete to perform the delete operation using $id
 
+<?php
+
+include 'connect.php';
+//$_GET used  for get the data from url
+if(isset($_GET['deleteid'])){
+    $id = $_GET['deleteid'];
+
+    $sql = "delete from `crud` where id=$id";
+    $result=mysqli_query($con, $sql);
+    if($result){
+        //echo "Delete successfully";
+        //redirect to the display page
+        header('location:display.php');
+    }
+    else{
+        die(mysqli_error($con));
+    }
+}
+
+?>
