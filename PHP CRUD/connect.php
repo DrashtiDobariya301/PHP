3433de2f66de33a85d78bb0e23ecf00bf2cddaff
Drashti Dobariya <114356720+DrashtiDobariya301@users.noreply.github.com>
2023-06-30 15:28:11 +0530

    Update connect.php

--- PHP CRUD/connect.php
+++ PHP CRUD/connect.php
@@ -1,1 +1,13 @@
+#establish the connection between the phpmyadmin database and php file
 
+<?php
+
+$con = new mysqli('localhost' , 'root' , 'Za81as@#1212' , 'crud_operation');
+if(!$con){
+
+    die(mysqli_error($con));
+}
+
+
+
+?>
